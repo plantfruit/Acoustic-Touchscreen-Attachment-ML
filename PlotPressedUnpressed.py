255856e64806a2eb102375331d466ca948b7f46b
Kevin Xu <60991949+plantfruit@users.noreply.github.com>
2025-02-23 15:28:45 -0500

    1D and 2D Sample Graph Figures
    Create figures that show the representative sample plots of FFT and time domain of chirp segment, for 1D and 2D acoustic tactile sensor

--- PlotPressedUnpressed.py
+++ PlotPressedUnpressed.py
@@ -8,6 +8,9 @@ D2_unpressedTime = ['Final Data/2D_time1U.txt', 'Final Data/2D_time2U.txt', 'Fin
 D2_pressedFFT = ['Final Data/2D_fft1P.txt', 'Final Data/2D_fft2P.txt', 'Final Data/2D_fft3P.txt']
 D2_unpressedFFT = ['Final Data/2D_fft1U.txt', 'Final Data/2D_fft2U.txt', 'Final Data/2D_fft3U.txt']
 
+D2v2_pressedFFT = ['Final Data/2D_v2_fft1P.txt', 'Final Data/2D_v2_fft2P.txt', 'Final Data/2D_v2_fft3P.txt']
+D2v2_pressedTime = ['Final Data/2D_v2_time1P.txt', 'Final Data/2D_v2_time2P.txt', 'Final Data/2D_v2_time3P.txt']
+
 D1_pressedTime = ['Final Data/1D_timeP.txt']
 D1_unpressedTime = ['Final Data/1D_timeU.txt']
 D1_pressedFFT = ['Final Data/1D_fftP.txt']
@@ -22,13 +25,13 @@ D2obj_pressedFFT = ['Final Data/2Dobj_1_fftP.txt','Final Data/2Dobj_2_fftP.txt',
 D2obj_pressedTime = ['Final Data/2Dobj_1_timeP.txt', 'Final Data/2Dobj_2_timeP.txt', 'Final Data/2Dobj_3_timeP.txt']
 
 # Parameters
-pressData = D1obj_pressedFFT
-unpressData = D1obj_unpressedFFT
+pressData = D2v2_pressedFFT
+unpressData = D2_unpressedFFT
 legends = ["Pressed", "Unpressed"] # List to hold custom legends
-ylim = [30, 120] #[50, 120] #[-12e3, 8e3]
+ylim = [30, 120] #[-100, 100] #[30, 120] # [50, 120] #[-12e3, 8e3] # [-33e3, 33e3]
 xName = 'Frequency (kHz)' #'Time (s)' #'Frequency (kHz)'
 yName = 'Magnitude (dB)' #'Magnitude' #'Magnitude (dB)'
-freqWindow = [2.5, 20]
+freqWindow = [5, 21] # [2.5, 20] for 1D, [5 21] for 2D
 fftOrTime = True  # True - FFT, False - Time domain
 
 
@@ -45,6 +48,7 @@ for file_path in pressData:
     # Load the data from the text file
     pressLine = np.loadtxt(file_path)
     unpressLine = np.loadtxt(unpressData[counter])
+    print(np.shape(pressLine))
     
     # Ensure the data is 1D
 ##    if data.ndim != 1:
@@ -54,8 +58,14 @@ for file_path in pressData:
     # Generate x-axis in seconds
     if (not(fftOrTime)):
         x = np.arange(1, len(pressLine) + 1) / fs * 1e3
+        pressSign = np.sign(pressLine)
+        unpressSign = np.sign(unpressLine)
+        pressLine = 10*np.log10(abs(pressLine)) #10*np.log10(pressLine - min(pressLine) + 1) #pressLine - min(pressLine) + 1
+        unpressLine = 10*np.log10(abs(unpressLine)) #10*np.log10(unpressLine - min(unpressLine) + 1) #unpressLine - min(unpressLine) + 1
+        pressLine = pressSign * pressLine
+        unpressLine = unpressSign * unpressLine
     else:
-        x = np.linspace(freqWindow[0], freqWindow[1], len(pressLine))
+        x = np.linspace(freqWindow[0], freqWindow[1], len(pressLine))        
 
     # Plot the data
     plt.plot(x, pressLine)  # Use file name as legend
@@ -72,7 +82,8 @@ for file_path in pressData:
     plt.xlabel(xName)
     plt.ylabel(yName)
     plt.legend(legends, loc='upper right')  # Adjust legend position
-    plt.grid(True)
+    #plt.grid(True)
+    plt.savefig('figure'+str(counter)+'.pdf')
     plt.show()
     
     counter = counter + 1
