bf2bd6eca34210b1a38b358b9822873c0d29b82b
Kevin Xu <60991949+plantfruit@users.noreply.github.com>
2025-02-27 22:51:56 -0500

    Symmetry Plot Update

--- PlotSymmetry.py
+++ PlotSymmetry.py
@@ -26,13 +26,14 @@ D1obj_unpressedFFT = ['Final Data/1D_6obj_fftU.txt']
 D2obj_pressedFFT = ['Final Data/2Dobj_1_fftP.txt','Final Data/2Dobj_2_fftP.txt','Final Data/2Dobj_3_fftP.txt']
 D2obj_pressedTime = ['Final Data/2Dobj_1_timeP.txt', 'Final Data/2Dobj_2_timeP.txt', 'Final Data/2Dobj_3_timeP.txt']
 
-Sym_pressed = ['1D Rows/1cm.txt', '1D Rows/2cm.txt', '1D Rows/3cm.txt', '1D Rows/4cm.txt']
+Sym_pressed = ['1D Rows/1cm.txt', '1D Rows/3cm.txt', '1D Rows/7cm.txt', '1D Rows/9cm.txt']
 Sym_unpressed = ['1D Rows/9cm.txt', '1D Rows/8cm.txt', '1D Rows/7cm.txt', '1D Rows/6cm.txt']
 
 # Parameters
 pressData = Sym_pressed
 unpressData = Sym_unpressed
-legends = ["1 cm, 2 cm, 3 cm, 4 cm", "6 cm, 7 cm, 8 cm, 9 cm"] # List to hold custom legends
+legends = ["1 cm", "3 cm", "7 cm", "9 cm"]
+#legends = ["1 cm, 2 cm, 3 cm, 4 cm", "6 cm, 7 cm, 8 cm, 9 cm"] # List to hold custom legends
 ylim = [30, 120] #[-60, 60] #[30, 120] # [50, 120] #[-12e3, 8e3] # [-33e3, 33e3]
 xName = 'Frequency (kHz)' #'Time (ms)' #'Frequency (kHz)'
 yName = 'Magnitude (dB)' #'Magnitude' #'Magnitude (dB)'
@@ -43,15 +44,20 @@ plotPeaks = True
 labelFontsize = 32
 textFontsize = 26
 
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+lineStyles = ['-', '--', '-.', ':']  
+
 # Sampling frequency
 fs = 48e3  # in Hz       
 # Processing begins                  
 
+
+# Initialize figure for the plot
+plt.figure(figsize=(12, 9))
 # Iterate over each file and plot its data
 counter = 0
 for file_path in pressData:
-    # Initialize figure for the plot
-    plt.figure(figsize=(12, 9))
+    
 
     # Load the data from the text file
     pressLine = np.loadtxt(file_path)
@@ -76,8 +82,8 @@ for file_path in pressData:
         x = np.linspace(freqWindow[0], freqWindow[1], len(pressLine))        
 
     # Plot the data
-    plt.plot(x, pressLine, color = 'blue')  # Use file name as legend
-    plt.plot(x, unpressLine, color = 'orange')
+    plt.plot(x, pressLine, color=colors[counter % len(colors)], linestyle = lineStyles[counter])  # Use file name as legend
+    #plt.plot(x, unpressLine, color = 'orange')
 
     # Optional: Customize legend names
     #legends.append(legends)
@@ -86,8 +92,8 @@ for file_path in pressData:
         markerSize = 100
         pressPeaks, _ = find_peaks(pressLine, prominence = 2)
         unpressPeaks, _ = find_peaks(unpressLine)
-        plt.scatter(x[pressPeaks], pressLine[pressPeaks], color = 'red', s = markerSize)
-        plt.scatter(x[unpressPeaks], unpressLine[unpressPeaks], color = 'green', s = markerSize)
+        #plt.scatter(x[pressPeaks], pressLine[pressPeaks], color = 'red', s = markerSize)
+        #plt.scatter(x[unpressPeaks], unpressLine[unpressPeaks], color = 'green', s = markerSize)
         print(x[pressPeaks])
         print(pressLine[pressPeaks])
 
@@ -100,9 +106,13 @@ for file_path in pressData:
     plt.ylabel(yName, fontsize = labelFontsize)    
     plt.xticks(fontsize = textFontsize)
     plt.yticks(fontsize = textFontsize)
-    plt.legend(legends, loc='upper right', fontsize = textFontsize)  # Adjust legend position
+    plt.legend(legends)
+    #plt.legend(legends, loc='upper right', fontsize = textFontsize)  # Adjust legend position
     #plt.grid(True)
-    plt.savefig('figure'+str(counter)+'.pdf')
-    plt.show()    
+      
     
     counter = counter + 1
+
+
+plt.savefig('figure'+str(counter)+'.pdf')
+plt.show()  
